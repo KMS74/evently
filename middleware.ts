7a47cd9b583d2728ef9a015c56fa36677a2b76c7
Karem Shabana <km425360@gmail.com>
2024-07-13 19:39:32 +0300

    refactor: Update middleware routes to use absolute paths

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/events:id",
-  "api/webhook/clerk",
-  "api/webhook/stripe",
+  "/api/webhook/clerk",
+  "/api/webhook/stripe",
   "api/webhook/uploadthing",
   "/sign-in(.*)",
   "/sign-up(.*)",
